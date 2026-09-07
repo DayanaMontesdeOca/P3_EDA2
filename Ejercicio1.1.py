b7630bc26e908c4c84548fd8d5d1273f7e958d81
DayanaMontesdeOca <fi.montesdeoca.unam@gmail.com>
2026-09-07 08:47:22 -0600

    Refactor countingSort with dynamic k and output
    Refactor counting sort to include dynamic k calculation and improve output formatting.

--- Ejercicio1.1.py
+++ Ejercicio1.1.py
@@ -1,20 +1,29 @@
 def countingSort(A, k):
-    C=[0 for _ in range(k + 1)]
-    B=[0 for _ in range(len(A))]
-    for j in range(0,len(A)):
-        C[A[j]]= C[A[j]]+1
+    C = [0 for _ in range(k + 1)]
+    B = [0 for _ in range(len(A))]
+    
+    for j in range(0, len(A)):
+        C[A[j]] = C[A[j]] + 1
 
     for i in range(1, k + 1):
-        C[i]=C[i]+C[i - 1]
+        C[i] = C[i] + C[i - 1]
 
     for j in range(len(A) - 1, -1, -1):
         B[C[A[j]] - 1] = A[j]
-        C[A[j]]= C[A[j]]-1
+        C[A[j]] = C[A[j]] - 1
+
     return B
 
-datos = [14, 3, 20, 0, 7, 14, 18, 2, 7, 11]
-k = max(datos) #k dinamico
+def dinamico(A):
+    k = max(A)
+    if not (10 < k < 30):
+        print(f"Error")
+    return k
 
-resultado = countingSort(datos, k)
-print("Lista desordenada:", datos)
-print("Lista ordenada:", resultado)
+A = [14, 3, 20, 0, 7, 14, 18, 2, 7, 11]
+k = dinamico(A)
+B = countingSort(A, k)
+
+print("Lista original A: ", A)
+print("Valor de k:       ", k)
+print("Lista ordenada B: ", B)
